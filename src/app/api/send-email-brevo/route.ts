30b20337b67d22a6238f10b81a1bf841b6045afb
gurnit <gurjiitsingh@gmail.com>
2025-04-24 17:18:40 +0530

    console error

--- src/app/api/send-email-brevo/route.ts
+++ src/app/api/send-email-brevo/route.ts
@@ -5,18 +5,28 @@ export async function POST(req: Request) {
 // secrit key
   // 
 console.log("email info-------------",  to, subject, message,process.env.BREVO_HOST,process.env.BREVO_USER,process.env.BREVO_PASS)
-  const transporter = nodemailer.createTransport({
-    host: process.env.BREVO_HOST,
-    port: 587,
-    auth: {
-      user: process.env.BREVO_USER, // Brevo verified sender
-      pass:  process.env.BREVO_PASS,
-    },
-  });
+ 
+const transporter = nodemailer.createTransport({
+  host: process.env.BREVO_HOST,
+  port: 587,
+  secure: false, // Use TLS
+  auth: {
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
+  },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("Error with SMTP connection:", error);
+  } else {
+    console.log("SMTP connection successful:", success);
+  }
+});
 
   try {
     await transporter.sendMail({
-      from: `"Masala GF" ${process.env.BREVO_USER}`,
+      from: `"Masala GF" <${process.env.BREVO_USER}>`,
       to,
       subject,
       text: message,
@@ -25,8 +35,10 @@ console.log("email info-------------",  to, subject, message,process.env.BREVO_H
     return new Response(JSON.stringify({ success: true }));
   } catch (err) {
     console.error('Email error:', err);
-    return new Response(JSON.stringify({ error: 'Failed to send email' }), {
+    // console.error('Error details:', err.message || err);
+    return new Response(JSON.stringify({ error: 'Failed to send email', details: err }), {
       status: 500,
     });
   }
+  
 }
